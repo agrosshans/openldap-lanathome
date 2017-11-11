7ec3ca90a458a2bdf5c291a4038fe153459bd17b
agrosshans <aurelien.grosshans@gmail.com>
2017-11-11 11:17:24 +0100

    Add files via upload

---
+++ provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -f /etc/debian_version ]; then
+  sudo apt-get update
+  sudo apt-get install -y git python-pip python-dev
+  sudo apt-get update
+  sudo apt-get install software-properties-common
+  sudo apt-add-repository ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get install ansible
+elif [ -f /etc/redhat-release ]; then
+#  rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+  yum install -y epel-release
+  yum install -y git python-pip python-devel python-dnf
+fi
+sudo pip install ansible
+cd roles/ ; git clone git@github.com:agrosshans/openldap.git
+ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
