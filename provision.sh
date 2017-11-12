49a7e798c1d6f3f2e2851e5ef568e936a1344270
Aurelien Grosshans <aurelien.grosshans@gmail.com>
2017-11-12 20:51:00 +0100

    2

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f /etc/debian_version ]; then
   sudo apt-get update
-  sudo apt-get install -y git python-pip python-dev ansible
+  sudo apt-get install -y git python-pip python-dev
   sudo apt-get install software-properties-common
   sudo apt-add-repository ppa:ansible/ansible
   sudo apt-get install ansible
@@ -11,5 +11,5 @@ elif [ -f /etc/redhat-release ]; then
   yum install -y git python-pip python-devel python-dnf
 fi
 
-cd roles/ ; git clone https://github.com/agrosshans/openldap-lanathome.git
-ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
+cd /vagrant ; git clone https://github.com/agrosshans/openldap-lanathome.git
+ansible-playbook -i "localhost," -c local /vagrant/ansible-playbook.yml
