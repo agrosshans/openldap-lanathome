aa4f439e39d5070892030d13cac180216e1a32f2
Aurelien Grosshans <aurelien.grosshans@gmail.com>
2017-11-18 06:56:47 +0100

    1

--- roles/mrlesmithjr.openldap/provision.sh
+++ roles/mrlesmithjr.openldap/provision.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 if [ -f /etc/debian_version ]; then
-  sudo apt-get update
   sudo apt-get install -y git python-pip python-dev ansible
+  sudo apt-get install -y software-properties-common
+  sudo apt-add-repository -y ppa:ansible/ansible
   sudo apt-get update
-  sudo apt-get install software-properties-common
-  sudo apt-add-repository ppa:ansible/ansible
-  sudo apt-get update
-  sudo apt-get install ansible
+  sudo apt-get install -y ansible
+  sudo apt-get upgrade -y ansible
 elif [ -f /etc/redhat-release ]; then
 #  rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
   yum install -y epel-release
   yum install -y git python-pip python-devel python-dnf
 fi
-sudo pip install --upgrade pip
-sudo pip install ansible
-cd roles/ ; git clone https://github.com/agrosshans/openldap-lanathome.git
-ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
+
+
+cd /vagrant ; git clone https://github.com/agrosshans/openldap-lanathome.git
+ansible-playbook -i "localhost," -c local /vagrant/ansible-playbook.yml
